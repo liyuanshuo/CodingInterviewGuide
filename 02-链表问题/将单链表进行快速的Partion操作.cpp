da30efd47574b5914bdc953d13b5809e3d1b9dfc
liyuanshuo <1405024239@st.nuc.edu.cn>
2020-09-05 22:19:00 +0800

    为什么 人要像机器一样生活

--- 02-链表问题/将单链表进行快速的Partion操作.cpp
+++ 02-链表问题/将单链表进行快速的Partion操作.cpp
@@ -24,5 +24,85 @@ struct ListNode {
 
 class Solution {
 public:
-    
+    void arrPartition(vector<ListNode*> nodeArr, int pivot) {
+        int small = -1;
+        int big = nodeArr.size();
+        int idx = 0;
+        while (idx != big) {
+            if (nodeArr[idx]->val < pivot) {
+                swap(nodeArr[++small], nodeArr[idx++]);
+            } else if (nodeArr[idx]->val == pivot) {
+                idx++;
+            } else {
+                swap(nodeArr[--big], nodeArr[idx]);
+            }
+        }
+    }
+
+    ListNode* listPartition1(ListNode* head, int pivot) {
+        if (head == nullptr) {
+            return head;
+        }
+        ListNode* cur = head;
+        vector<ListNode*> nodeArr;
+        int i = 0;
+        while (cur != nullptr) {
+            ++i;
+            nodeArr.push_back(cur);
+            cur = cur->next;
+        }
+        arrPartition(nodeArr, pivot);
+        for (int i = 1; i != nodeArr.size(); ++i) {
+            nodeArr[i - 1]->next = nodeArr[i];
+        }
+        nodeArr[i - 1]->next = nullptr;
+        return nodeArr[0];
+    }
+
+    ListNode* listPartition2(ListNode* head, int pivot) {
+        ListNode* smallHead = nullptr;
+        ListNode* smallTail = nullptr;
+        ListNode* equalHead = nullptr;
+        ListNode* equalTail = nullptr;
+        ListNode* bigHead = nullptr;
+        ListNode* bigTail = nullptr;
+        ListNode* next = nullptr;
+        while (head != nullptr) {
+            next = head->next;
+            head->next = nullptr;
+            if (head->val < pivot) {
+                if (smallHead == nullptr) {
+                    smallHead = head;
+                } else {
+                    smallTail->next = head;
+                }
+                smallTail = head;
+            } else if (head->val == pivot) {
+                if (equalHead == nullptr) {
+                    equalHead = head;
+                } else {
+                    equalTail->next = head;
+                }
+                equalTail = head;
+            } else {
+                if (bigHead == nullptr) {
+                    bigHead = nullptr;
+                } else {
+                    bigTail->next = head;
+                }
+                bigTail = head;
+            }
+            head = next;
+        }
+        if (smallTail != nullptr) {
+            smallTail->next = equalHead;
+            equalTail = equalTail == nullptr ? smallTail : equalTail;
+        }
+        if (equalTail != nullptr) {
+            equalTail->next = bigHead;
+        }
+        return smallHead != nullptr
+                    ? smallHead
+                    : equalHead != nullptr ? equalHead : bigHead;
+    }
 };
